bed860122bd7e811e14ad139865ec09ecc21c564
deepak <deepak@pointers@digitech.com>
2024-02-21 23:09:40 +0530

    changes in cv

--- src/components/Profile/CTA.jsx
+++ src/components/Profile/CTA.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import CV from '../../assets/deepak(cv).pdf'
-import {Link} from 'react-router-dom'
+import CV1 from '../../assets/deepak(cv).pdf'
+import CV from '../../assets/deepak(cv-1).pdf'
+import { Link } from 'react-router-dom'
 const CTA = () => {
   return (
     <div className="cta">
-        <a href={CV} className='btn' target="_blank">Download CV</a>
-        <Link to="/contact" className='btn btn-primary'>Let's Talk</Link>
+      <a href={CV} className='btn' target="_blank">Download CV</a>
+      <Link to="/contact" className='btn btn-primary'>Let's Talk</Link>
     </div>
   )
 }
